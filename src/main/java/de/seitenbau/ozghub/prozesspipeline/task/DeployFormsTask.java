2503acc31d54482ce8922a6f202f4439a8ed6b1e
Johannes Kern <johannes.kern@seitenbau.com>
2021-11-17 08:37:39 +0100

    Rename attribute formFiles to files
    SBW-22379

--- src/main/java/de/seitenbau/ozghub/prozesspipeline/task/DeployFormsTask.java
+++ src/main/java/de/seitenbau/ozghub/prozesspipeline/task/DeployFormsTask.java
@@ -12,18 +12,18 @@ import lombok.Setter;
 @Setter
 public class DeployFormsTask extends DefaultPluginTask
 {
-   /**
+  /**
    * Pfad zum Ordner mit Formular-Dateien. Ist kein Pfad angegeben, werden die Dateien im Order
    * forms im aktuellen Projekt verwendet.
    */
   @Input
   @Optional
-  String formFiles;
+  String files;
 
   @TaskAction
   public void run()
   {
-    DeployFormsHandler handler = new DeployFormsHandler(getEnvironment(), getProjectDir(), formFiles);
+    DeployFormsHandler handler = new DeployFormsHandler(getEnvironment(), getProjectDir(), files);
 
     handler.deploy();
   }
