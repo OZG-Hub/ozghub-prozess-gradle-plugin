0bfaa6d34e0942fc2bdf79ee8d9eaab92cad410f
Johannes Kern <johannes.kern@seitenbau.com>
2021-11-23 18:00:48 +0100

    Add test
    SBW-22381

---
+++ src/test/java/de/seitenbau/ozghub/prozesspipeline/handler/ListAppender.java
@@ -0,0 +1,46 @@
+package de.seitenbau.ozghub.prozesspipeline.handler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.logging.log4j.core.Appender;
+import org.apache.logging.log4j.core.Core;
+import org.apache.logging.log4j.core.Filter;
+import org.apache.logging.log4j.core.LogEvent;
+import org.apache.logging.log4j.core.appender.AbstractAppender;
+import org.apache.logging.log4j.core.config.Property;
+import org.apache.logging.log4j.core.config.plugins.Plugin;
+import org.apache.logging.log4j.core.config.plugins.PluginAttribute;
+import org.apache.logging.log4j.core.config.plugins.PluginElement;
+import org.apache.logging.log4j.core.config.plugins.PluginFactory;
+
+import lombok.Getter;
+
+@Plugin(
+    name = "ListAppender",
+    category = Core.CATEGORY_NAME,
+    elementType = Appender.ELEMENT_TYPE)
+public class ListAppender extends AbstractAppender
+{
+  @Getter
+  private List<String> eventList = new ArrayList<>();
+
+  protected ListAppender(String name, Filter filter)
+  {
+    super(name, filter, null, true, Property.EMPTY_ARRAY);
+  }
+
+  @PluginFactory
+  public static ListAppender createAppender(
+      @PluginAttribute("name") String name,
+      @PluginElement("Filter") Filter filter)
+  {
+    return new ListAppender(name, filter);
+  }
+
+  @Override
+  public void append(LogEvent event)
+  {
+    eventList.add(event.getLevel() + " " + event.getMessage().getFormattedMessage());
+  }
+}
---
+++ src/test/java/de/seitenbau/ozghub/prozesspipeline/handler/ListProcessesHandlerTest.java
@@ -0,0 +1,206 @@
+package de.seitenbau.ozghub.prozesspipeline.handler;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
+import java.util.Base64;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.core.LoggerContext;
+import org.apache.logging.log4j.core.config.Configuration;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sun.net.httpserver.HttpServer;
+
+import de.seitenbau.ozghub.prozesspipeline.common.Environment;
+import de.seitenbau.ozghub.prozesspipeline.common.HTTPHeaderKeys;
+import de.seitenbau.ozghub.prozesspipeline.integrationtest.HttpHandler;
+import de.seitenbau.ozghub.prozesspipeline.integrationtest.HttpServerFactory;
+import de.seitenbau.ozghub.prozesspipeline.model.response.Deployment;
+import de.seitenbau.ozghub.prozesspipeline.model.response.DeploymentList;
+import lombok.SneakyThrows;
+
+public class ListProcessesHandlerTest
+{
+  protected static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+
+  private static final String DATE_FORMAT = "YYYY-MM-dd hh:mm:ss";
+
+  private HttpServer httpServer = null;
+
+  private ListProcessesHandler sut;
+
+  private ListAppender listAppender;
+
+  @AfterEach
+  private void after()
+  {
+    if (httpServer != null)
+    {
+      httpServer.stop(0);
+    }
+  }
+
+  @Test
+  public void listProcesses()
+  {
+    // arrange
+    prepareLogging();
+
+    Date deploymentDate = new Date();
+
+    DeploymentList deploymentList = prepareDeployment(deploymentDate);
+
+    HttpHandler httpHandler = createAndStartHttpServer(deploymentList);
+
+    String url = "http://localhost:" + httpServer.getAddress().getPort();
+    Environment env = new Environment(url, "foo1", "bar1");
+
+    sut = new ListProcessesHandler(env);
+
+    // act
+    sut.list();
+
+    // assert
+    assertThat(httpHandler.countRequests()).isEqualTo(1);
+    assertThat(httpHandler.getResponseCode()).isEqualTo(200);
+
+    HttpHandler.Request actualRequest = httpHandler.getRequest();
+
+    assertRequest(actualRequest);
+    assertRequestHeaders(actualRequest, env);
+
+    List<String> actualLogMessages = listAppender.getEventList();
+    assertDeploymentLogMessage(actualLogMessages, deploymentDate, deploymentList.getValue().get(0));
+  }
+
+  @Test
+  public void listProcesses_deploymentList_incomplete()
+  {
+    // arrange
+    prepareLogging();
+
+    Date deploymentDate = new Date();
+
+    DeploymentList deploymentList = prepareDeployment(deploymentDate);
+    deploymentList.setComplete(false);
+
+    HttpHandler httpHandler = createAndStartHttpServer(deploymentList);
+
+    String url = "http://localhost:" + httpServer.getAddress().getPort();
+    Environment env = new Environment(url, "foo1", "bar1");
+
+    sut = new ListProcessesHandler(env);
+
+    // act
+    sut.list();
+
+    // assert
+    assertThat(httpHandler.countRequests()).isEqualTo(1);
+    assertThat(httpHandler.getResponseCode()).isEqualTo(200);
+
+    HttpHandler.Request actualRequest = httpHandler.getRequest();
+
+    assertRequest(actualRequest);
+    assertRequestHeaders(actualRequest, env);
+
+    List<String> actualLogMessages = listAppender.getEventList();
+    assertDeploymentLogMessage(actualLogMessages, deploymentDate, deploymentList.getValue().get(0));
+    assertThat(actualLogMessages).contains(
+        "WARN Es konnten nicht alle Deployments von allen Prozessengines abgerufen werden.");
+  }
+
+  private DeploymentList prepareDeployment(Date deploymentDate)
+  {
+    TreeMap<String, String> keysAndNames = new TreeMap<>();
+    keysAndNames.put("processKey1", "processName1");
+    keysAndNames.put("processKey2", "processName2");
+
+    Deployment deployment1 = Deployment.builder()
+        .deploymentDate(deploymentDate)
+        .deploymentId("deploymentId1")
+        .deploymentName("deploymentName1")
+        .processDefinitionKeysAndNames(keysAndNames)
+        .build();
+
+    return DeploymentList.builder().complete(true).value(List.of(deployment1)).build();
+  }
+
+  private void prepareLogging()
+  {
+    final LoggerContext ctx = (LoggerContext) LogManager.getContext(false);
+    final Configuration config = ctx.getConfiguration();
+    listAppender = (ListAppender) config.getAppenders().get("ListAppender");
+  }
+
+  private void assertDeploymentLogMessage(List<String> actualLogMessages, Date deploymentDate,
+      Deployment deployment1)
+  {
+    String expectedLogMessage = createExpectedLogMessage(deploymentDate, deployment1);
+    assertThat(actualLogMessages).contains(expectedLogMessage);
+  }
+
+  private String createExpectedLogMessage(Date deploymentDate, Deployment deployment1)
+  {
+    SimpleDateFormat format = new SimpleDateFormat(DATE_FORMAT);
+    String expectedDeploymentDateString = format.format(deploymentDate);
+
+    return "INFO Vorhandene Deployments:\n" +
+        expectedDeploymentDateString +
+        " " +
+        deployment1.getDeploymentName() +
+        " " +
+        "Deployment-ID: " +
+        deployment1.getDeploymentId() +
+        "\n" +
+        " - " +
+        "processKey1" +
+        " " +
+        "processName1" +
+        "\n" +
+        " - " +
+        "processKey2" +
+        " " +
+        "processName2" +
+        "\n";
+  }
+
+  private void assertRequest(HttpHandler.Request request)
+  {
+    assertThat(request.getRequestMethod()).isEqualTo("GET");
+    assertThat(request.getPath()).isEqualTo(ListProcessesHandler.API_PATH);
+    assertThat(request.getQuery()).isNull();
+  }
+
+  private void assertRequestHeaders(HttpHandler.Request request, Environment env)
+  {
+    Map<String, List<String>> headers = request.getHeaders();
+    assertThat(headers).containsEntry(HTTPHeaderKeys.CONTENT_TYPE, List.of("application/json"));
+
+    String tmp = env.getUser() + ':' + env.getPassword();
+    String auth = "Basic " + Base64.getEncoder().encodeToString(tmp.getBytes(StandardCharsets.UTF_8));
+    assertThat(headers).containsEntry(HTTPHeaderKeys.AUTHORIZATION, List.of(auth));
+  }
+
+  private HttpHandler createAndStartHttpServer(DeploymentList deploymentList)
+  {
+    HttpHandler httpHandler = createHttpHandler(deploymentList);
+    httpServer =
+        HttpServerFactory.createAndStartHttpServer(ListProcessesHandler.API_PATH, httpHandler);
+
+    return httpHandler;
+  }
+
+  @SneakyThrows
+  private HttpHandler createHttpHandler(DeploymentList deploymentList)
+  {
+    return new HttpHandler(200, OBJECT_MAPPER.writeValueAsBytes(deploymentList));
+  }
+}
