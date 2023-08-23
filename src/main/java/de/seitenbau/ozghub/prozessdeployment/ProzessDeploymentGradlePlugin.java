d9254514a32ad39fccf80ef613da77197880a792
Maksim Sandybekov <maksim.sandybekov@seitenbau.com>
2023-08-23 16:30:44 +0200

    cleanup task registration and give tasks group
    SBW-27772
    
    - add group name for tasks to be collected
    under common name

--- src/main/java/de/seitenbau/ozghub/prozessdeployment/ProzessDeploymentGradlePlugin.java
+++ src/main/java/de/seitenbau/ozghub/prozessdeployment/ProzessDeploymentGradlePlugin.java
@@ -4,6 +4,7 @@ import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.tasks.TaskContainer;
 
+import de.seitenbau.ozghub.prozessdeployment.task.DefaultPluginTask;
 import de.seitenbau.ozghub.prozessdeployment.task.DeployFormsTask;
 import de.seitenbau.ozghub.prozessdeployment.task.DeployProcessTask;
 import de.seitenbau.ozghub.prozessdeployment.task.EncryptParameterValueTask;
@@ -14,29 +15,53 @@ import de.seitenbau.ozghub.prozessdeployment.task.UndeployProcessTask;
 
 public class ProzessDeploymentGradlePlugin implements Plugin<Project>
 {
+  private final static String TASK_GROUP_NAME = "OZG-Hub";
+
+  private TaskContainer taskContainer;
+
   public void apply(Project project)
   {
-    TaskContainer tasks = project.getTasks();
+    taskContainer = project.getTasks();
+    createTask(
+        "deployProcess",
+        DeployProcessTask.class,
+        "Deployt Prozessdefinitionen");
 
-    tasks.create("deployProcess", DeployProcessTask.class, (task) -> {
-    }).setDescription("Deployt Prozessdefinitionen");
+    createTask(
+        "deployForms",
+        DeployFormsTask.class,
+        "Deployt Formulare");
 
-    tasks.create("deployForms", DeployFormsTask.class, (task) -> {
-    }).setDescription("Deployt Formulare");
+    createTask(
+        "undeployProcess",
+        UndeployProcessTask.class,
+        "Löscht ein Prozess-Deployment");
 
-    tasks.create("undeployProcess", UndeployProcessTask.class, (task) -> {
-    }).setDescription("Löscht ein Prozess-Deployment");
+    createTask(
+        "undeployForm",
+        UndeployFormTask.class,
+        "Löscht ein Formular-Deployment");
 
-    tasks.create("undeployForm", UndeployFormTask.class, (task) -> {
-    }).setDescription("Löscht ein Formular-Deployment");
+    createTask(
+        "listProcesses",
+        ListProcessesTask.class,
+        "Zeigt eine Liste aller deployten Prozesse an");
 
-    tasks.create("listProcesses", ListProcessesTask.class, (task) -> {
-    }).setDescription("Zeigt eine Liste aller deployten Prozesse an");
+    createTask(
+        "listForms",
+        ListFormsTask.class,
+        "Zeigt eine Liste aller deployten Formulare an");
 
-    tasks.create("listForms", ListFormsTask.class, (task) -> {
-    }).setDescription("Zeigt eine Liste aller deployten Formulare an");
+    createTask(
+        "encryptParameterValue",
+        EncryptParameterValueTask.class,
+        "Verschlüsselt einen Prozessparameterwert");
+  }
 
-    tasks.create("encryptParameterValue", EncryptParameterValueTask.class, (task) -> {
-    }).setDescription("Verschlüsselt einen Prozessparameterwert");
+  private <K extends DefaultPluginTask> void createTask(String name, Class<K> aClass, String description)
+  {
+    K createdTask = taskContainer.create(name, aClass, (task) -> {});
+    createdTask.setDescription(description);
+    createdTask.setGroup(TASK_GROUP_NAME);
   }
 }
