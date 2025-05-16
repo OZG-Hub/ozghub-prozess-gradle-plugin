6a01a391125a4d0393033bf15982a01fab96dba1
Maksim Sandybekov <maksim.sandybekov@seitenbau.com>
2025-05-16 10:58:04 +0200

    Ignore unknown properties
    SBW-32396

--- src/main/java/de/seitenbau/ozghub/prozessdeployment/model/response/ProcessEngine.java
+++ src/main/java/de/seitenbau/ozghub/prozessdeployment/model/response/ProcessEngine.java
@@ -1,5 +1,7 @@
 package de.seitenbau.ozghub.prozessdeployment.model.response;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,11 +11,16 @@ import lombok.NoArgsConstructor;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class ProcessEngine
 {
-  /** ID of the process-engine. Can be used to specify the engine for deployment. */
+  /**
+   * ID of the process-engine. Can be used to specify the engine for deployment.
+   */
   private String id;
 
-  /** Name of the process-engine. */
+  /**
+   * Name of the process-engine.
+   */
   private String name;
 }
